91a2e73d88696353b907c7ed62828fac6d347953
Felipev98 <felipe_v_spd@hotmail.com>
2021-06-08 01:43:02 -0400

    Firebase y vista Registro añadidos

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey:"AIzaSyCDZzYIHyoD4l7Wo9Tjma3rgGfTBHuNCt4",
+    authDomain: "bootcamp-25600.firebaseapp.com",
+    projectId: "bootcamp-25600",
+    storageBucket: "bootcamp-25600.appspot.com",
+    messagingSenderId: "853140846017",
+    appId:"1:853140846017:web:b57c700d7bfd138d4deb40",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore()
+const auth = firebase.auth()
+export{db,auth}
---
+++ src/views/Registro.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Registro de Usuario</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
