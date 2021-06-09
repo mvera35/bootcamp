e0f0bb4eb41363d2d6b58bf7be44256ceb1479f7
mvera35 <mvera35@uabc.edu.mx>
2021-06-08 19:34:31 -0700

    Creación de vista usuario e inclusión de componentes de vuetify en el App.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,38 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/registro',
-    name: 'Registro',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Registro.vue')
-  }
-]
+	{
+		path: "/",
+		name: "Home",
+		component: Home,
+	},
+	{
+		path: "/registro",
+		name: "Registro",
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () =>
+			import(
+				/* webpackChunkName: "about" */ "../views/Registro.vue"
+			),
+	},
+	{
+		path: "/perfil",
+		name: "perfil",
+		component: () =>
+			import(
+				/* webpackChunkName: "about" */ "../views/PerfilUsuario.vue"
+			),
+	},
+];
 
 const router = new VueRouter({
-  routes
-})
+	routes,
+});
 
-export default router
+export default router;
---
+++ src/views/PerfilUsuario.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Perfil de usuario</h1>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
