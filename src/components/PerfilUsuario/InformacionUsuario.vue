110106bf4492417c993a7c6bb66f003aed888f31
mvera35 <mvera35@uabc.edu.mx>
2021-07-23 12:34:25 -0700

    Creación de los archivos CartaEmpresa y CartaPuesto

--- src/components/PerfilUsuario/InformacionUsuario.vue
+++ src/components/PerfilUsuario/InformacionUsuario.vue
@@ -1,42 +1,40 @@
 <template>
-    <v-card class="mx-auto" color="#f8f8fa" elevation="0" width="85%">
-        <v-card-title class="grey--text font-weight-bold text-h6">
-            Nombre de sección
-        </v-card-title>
-        <v-card-text>
-            <div class="div-line"></div>
-            <p class="black--text font-weight-regular text-justify text-h6">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-
-            </p>
-        </v-card-text>
-        <v-card-actions>
-            <Editar absolute right bottom/>
-        </v-card-actions>
-    </v-card>
+  <v-card class="mx-auto" color="#f8f8fa" elevation="0" width="85%">
+    <v-card-title class="grey--text font-weight-bold text-h6">
+      Nombre de sección
+    </v-card-title>
+    <v-card-text>
+      <div class="div-line"></div>
+      <p class="black--text font-weight-regular text-justify text-h6">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </v-card-text>
+    <v-card-actions>
+      <Editar absolute right bottom />
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
 import Editar from "@/components/Buttons/Editar";
 
 export default {
-    name: "Informacion",
-    components: {
-        Editar,
-    },
+  name: "Informacion",
+  components: {
+    Editar,
+  },
 };
 </script>
 
 <style scoped>
 .div-line {
-    border-bottom: 1px solid #9e9e9e;
-    margin: 0 0 15px 0;
+  border-bottom: 1px solid #9e9e9e;
+  margin: 0 0 15px 0;
 }
-
 </style>
